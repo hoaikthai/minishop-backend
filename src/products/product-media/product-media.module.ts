6d937f502c1fd5dc220f8dcc2b2f68c5326d5820
Hoai Thai <khanhhoai95@gmail.com>
2021-02-22 20:00:50 +0700

    fix: repository injection issues

--- src/products/product-media/product-media.module.ts
+++ src/products/product-media/product-media.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductMediaController } from './product-media.controller';
 import { ProductMediaService } from './product-media.service';
+import { ProductMediaRepository } from './product-media.repository';
 
 @Module({
   controllers: [ProductMediaController],
-  providers: [ProductMediaService]
+  imports: [TypeOrmModule.forFeature([ProductMediaRepository])],
+  exports: [TypeOrmModule],
+  providers: [ProductMediaService],
 })
 export class ProductMediaModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ProductMediaModule } from './product-media/product-media.module';
+import { ProductsRepository } from './products.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ProductsRepository]),
+    ProductMediaModule,
+  ],
   providers: [ProductsService],
   controllers: [ProductsController],
-  imports: [ProductMediaModule]
 })
 export class ProductsModule {}
